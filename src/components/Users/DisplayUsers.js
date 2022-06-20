4529a4ef8ecf919dcb5ba09fee620c2ca54abbbb
Lucca Barroso <barrosolucca@gmail.com>
2022-06-20 09:35:39 -0300

    display users

--- src/components/Users/DisplayUsers.js
+++ src/components/Users/DisplayUsers.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Card from "../UI/Card";
 
+import styles from "./DisplayUsers.module.scss";
+
 const DisplayUsers = (props) => {
   return (
     <Card>
-      {props.users}
+      {props.users.map((user,index)=>{
+        return <div className={styles["userItem"]} key={(index+" "+Math.random()).toString()}>{user}</div>
+      })}
     </Card>
   )
 }
--- src/components/Users/FormUser.js
+++ src/components/Users/FormUser.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import Button from "../UI/Button";
 import Card from "../UI/Card";
 import styles from "./FormUser.module.scss";
 
@@ -9,6 +10,10 @@ const FormUser = (props) => {
   const CreateNewUserHandler = (e) => {
     e.preventDefault();
     console.log(`CreateNewUserHandler ${userName} ${userAge}`);
+    props.onCreateNewUser(userName, userAge);
+    setUserName("")
+    setUserAge("")
+
   }
   const changeNameHandler = (e) => {
     setUserName(e.target.value)
@@ -19,13 +24,13 @@ const FormUser = (props) => {
 
   return (
     <Card className={styles["form-user"]}>
-    <form onSubmit={CreateNewUserHandler} className={styles["form-user"]}>
-      <label for="fname">Name </label>
-      <input type="text" onChange={changeNameHandler} id="fname" name="fname" placeholder="Your name" />
-      <label for="lname">Age </label>
-      <input type="text" onChange={changeAgeHandler} id="lname" name="lname" placeholder="Your age" />
-      <input type="submit" value="Submit" />
-    </form>
+      <form onSubmit={CreateNewUserHandler} className={styles["form-user"]}>
+        <label>Name </label>
+        <input type="text" onChange={changeNameHandler} value={userName ? userName : ""} placeholder="Your name" />
+        <label for="lname">Age </label>
+        <input type="text" onChange={changeAgeHandler} value={userAge ? userAge : ""} placeholder="Your age" />
+        <Button type="submit" children={"Submit"}/>
+      </form>
     </Card>
   );
 };
